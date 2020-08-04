9b2754e4cfc6409a8637ee7aa24b7c0abdb7173d
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-08-03 22:55:12 -0400

    initial commit

---
+++ src/main/java/cl/pablosilvab/demobackendspringboot/DemoBackendSpringBootApplication.java
@@ -0,0 +1,13 @@
+package cl.pablosilvab.demobackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackendSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBackendSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/pablosilvab/demobackendspringboot/DemoBackendSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.pablosilvab.demobackendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBackendSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
