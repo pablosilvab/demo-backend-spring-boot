9f1926d773e7a08d54fd2d88fc7466a9d4ebd44f
Pablo Silva <pablonicolassilvabravo@gmail.com>
2025-03-07 02:00:10 -0300

    fix integration test

--- src/test/java/cl/pablosilvab/demobackendspringboot/controller/ProductControllerIntegrationTest.java
+++ src/test/java/cl/pablosilvab/demobackendspringboot/controller/ProductControllerIntegrationTest.java
@@ -2,6 +2,7 @@ package cl.pablosilvab.demobackendspringboot.controller;
 
 import cl.pablosilvab.demobackendspringboot.dto.model.ProductDTO;
 import cl.pablosilvab.demobackendspringboot.dto.request.ProductCreateDTO;
+import cl.pablosilvab.demobackendspringboot.entity.ProductType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,17 +11,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class ProductControllerIntegrationTest {
 
-    static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0")
+    @Container
+    private static final MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0")
             .withDatabaseName("testdb")
             .withUsername("test")
             .withPassword("test");
@@ -28,20 +35,17 @@ class ProductControllerIntegrationTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @BeforeEach
-    void setUp() {
-        mysqlContainer.start();
-
-
-        System.setProperty("spring.datasource.url", mysqlContainer.getJdbcUrl());
-        System.setProperty("spring.datasource.username", mysqlContainer.getUsername());
-        System.setProperty("spring.datasource.password", mysqlContainer.getPassword());
-        System.setProperty("spring.datasource.driver-class-name", mysqlContainer.getDriverClassName());
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mysqlContainer::getUsername);
+        registry.add("spring.datasource.password", mysqlContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", mysqlContainer::getDriverClassName);
     }
 
     @Test
     void testCreateProduct() {
-        ProductCreateDTO productCreateDTO = new ProductCreateDTO("Smartphone", "A new smartphone", 200000, 50);
+        ProductCreateDTO productCreateDTO = new ProductCreateDTO("Smartphone", "A new smartphone", 200000, 50, ProductType.ELECTRONIC.name());
 
         ResponseEntity<ProductDTO> response = restTemplate
                 .postForEntity("/api/v1/products/", productCreateDTO, ProductDTO.class);
